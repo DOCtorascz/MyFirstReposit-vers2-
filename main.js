942afd4490084cac0c9b77d6ca22109c71015ab2
Тимур <timur-skvorcov-666@mail.ru>
2023-01-23 14:33:28 +0500

    замена ссылок и удаление3

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 function posSize(message) {
  //удаление доменов второго уровня
  let array = `.ru .org .com .academy .accountant .accountants .active 
-   .actor .adult .aero .agency .airforce .apartments .app .archi .army .associates .asia .adult .aero 
-   .attorney .auction .audio .autos .biz .cat .coop .dance 
-   .edu .eus .family .fun .gov .info .int .jobs .mil .mobi .museum .name
-   .net .one .ong .onl .online .ooo
-   .organic .partners .parts .party .pharmacy .photo .photography .photos .physio .pics .pictures .feedback .pink 
-   .pizza .place .plumbing, .plus, .poker .porn .post .press 
-   .pro .productions .properties .property .qpon .racing .recipes .red .rehab .ren .rent .rentals
-   .report .republican .rest .review .reviews .rich .site .tel .trade .travel .xxx .xyz 
-   .yoga .zone,.ninja .art .moe .dev`
+ .actor .adult .aero .agency .airforce .apartments .app .archi .army .associates .asia .adult .aero 
+ .attorney .auction .audio .autos .biz .cat .coop .dance 
+ .edu .eus .family .fun .gov .info .int .jobs .mil .mobi .museum .name
+ .net .one .ong .onl .online .ooo
+ .organic .partners .parts .party .pharmacy .photo .photography .photos .physio .pics .pictures .feedback .pink 
+ .pizza .place .plumbing, .plus, .poker .porn .post .press 
+ .pro .productions .properties .property .qpon .racing .recipes .red .rehab .ren .rent .rentals
+ .report .republican .rest .review .reviews .rich .site .tel .trade .travel .xxx .xyz 
+ .yoga .zone,.ninja .art .moe .dev`
 
  let arrayReverse = array.split('')
  arrayReverse.reverse()
