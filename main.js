6d654ffcec4598401ffba5bfa9850efa6a960fda
Тимур <timur-skvorcov-666@mail.ru>
2023-01-23 12:41:21 +0500

    замена ссылок и удаление

--- main.js
+++ main.js
@@ -1,60 +1,65 @@
 function posSize(message) {
+	//удаление доменов второго уровня
+	let array = `.ru .org .com .academy .accountant .accountants .active 
+	.actor .adult .aero .agency .airforce .apartments .app .archi .army .associates .asia .adult .aero 
+	.attorney .auction .audio .autos .biz .cat .coop .dance 
+	.edu .eus .family .fun .gov .info .int .jobs .mil .mobi .museum .name
+	.net .one .ong .onl .online .ooo
+	.organic .partners .parts .party .pharmacy .photo .photography .photos .physio .pics .pictures .feedback .pink 
+	.pizza .place .plumbing, .plus, .poker .porn .post .press 
+	.pro .productions .properties .property .qpon .racing .recipes .red .rehab .ren .rent .rentals
+	.report .republican .rest .review .reviews .rich .site .tel .trade .travel .xxx .xyz 
+	.yoga .zone,.ninja .art .moe .dev`
+		
+	let arrayReverse = array.split('')
+	arrayReverse.reverse()
+	arrayReverse = arrayReverse.join('')
+	arrayReverse = arrayReverse.split(' ')
 
- let str = "";
- let strMess = message;
- let indx = message.indexOf("https://");
- let indxP = message.indexOf("http://");
- let indxW = message.indexOf("www");
+	let arrayMessage = message.split('')
+	arrayMessage.reverse()
+	arrayMessage = arrayMessage.join('')
 
- // На случай, если ссылок нет
- if (indx == -1 && indxP == -1 && indxW == -1) {
-   console.log(message.length);
-   return message.length;
- }
+	let result = ''
+	let arrayResult = []
+	let messageDuble = message
 
- //Если ссылки обнаружены
- for (let i = 0; i <= message.length - 1; i++) {
-   //Ищем индекс каждой ссылки
-   let indxHttps = message.indexOf("https://", i);
-   let indxResult = null;
+	let indxEquals = null
 
-   //Если индекс найден меняем ссылку
-   if (indxHttps > -1) {
-     str = message.slice(indxHttps);
-     indxResult = str.indexOf(" ");
-     str = str.slice(0, indxResult);
-     strMess = message.replace(str, `<a href="${str}">${str}</a>`);
-     break;
-   }
- }
+	for (let i = 0; i <= arrayReverse.length - 1; i++) {
 
- for (let j = 0; j <= strMess.length - 1; j++) {
-   //Ищем индекс каждой ссылки
-   let indxHttp = message.indexOf("http://", j);
-   let indxResult = null;
+		for (let j = 0; j <= messageDuble.length - 1; j++) {
+			let indx = arrayMessage.indexOf(arrayReverse[i], j)
+			let indxDoth = arrayMessage.indexOf('.', indx)
 
-   //Если индекс найден меняем ссылку
-   if (indxHttp > -1) {
-     str = message.slice(indxHttp);
-     indxResult = str.indexOf(" ");
-     str = str.slice(0, indxResult);
-     strMess = strMess.replace(str, `<a href="${str}">${str}</a>`);
-     break;
-   }
- }
+			if (indx != -1 && arrayMessage[indxDoth + 1] != ' ') {
+				let indxSpace = arrayMessage.indexOf(' ', indx)
+				let str = arrayMessage.slice(indx, indxSpace)
+				str = str.split('')
+				str.reverse()
+				str = str.join('')
+				arrayResult.push(str)
+				indxEquals = indx
+			}
+		} 
+	}
 
- for (let x = 0; x <= strMess.length - 1; x++) {
-   //Ищем индекс каждой ссылки
-   let indxWww = message.indexOf("www.", x);
-   let indxResult = null;
+	if (indxEquals == null) {
+		console.log(message)
+		return message
+	}
 
-   //Если индекс найден меняем ссылку
-   if (indxWww > -1) {
-     str = message.slice(indxWww);
-     strMess = strMess.replace(str, `<a href="${str}">${str}</a>`);
-     break;
-   }
- }
- console.log(strMess);
- return strMess;
+	let resultFilter = arrayResult.filter((item, index) => {
+		return arrayResult.indexOf(item) == index
+	})
+
+	for (let resultReplace = 0; resultReplace <= resultFilter.length - 1; resultReplace++) {
+		message = message.replace(resultFilter[resultReplace], `<a href="${resultFilter[resultReplace]}">${resultFilter[resultReplace]}</a>`)
+		result = message
+	}
+
+	console.log(result)
+	return result
 }
+posSize('Привет! fdsfds.porn asdas.porn dsfdsfhj.com');
+
