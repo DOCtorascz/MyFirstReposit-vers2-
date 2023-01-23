7651257dc914ee2747584b3c2858eb3888292659
Тимур <timur-skvorcov-666@mail.ru>
2023-01-23 12:56:58 +0500

    замена ссылок и удаление3

--- main.js
+++ main.js
@@ -1,65 +1,65 @@
-		function posSize(message) {
-			//удаление доменов второго уровня
-			let array = `.ru .org .com .academy .accountant .accountants .active 
-			.actor .adult .aero .agency .airforce .apartments .app .archi .army .associates .asia .adult .aero 
-			.attorney .auction .audio .autos .biz .cat .coop .dance 
-			.edu .eus .family .fun .gov .info .int .jobs .mil .mobi .museum .name
-			.net .one .ong .onl .online .ooo
-			.organic .partners .parts .party .pharmacy .photo .photography .photos .physio .pics .pictures .feedback .pink 
-			.pizza .place .plumbing, .plus, .poker .porn .post .press 
-			.pro .productions .properties .property .qpon .racing .recipes .red .rehab .ren .rent .rentals
-			.report .republican .rest .review .reviews .rich .site .tel .trade .travel .xxx .xyz 
-			.yoga .zone,.ninja .art .moe .dev`
-				
-			let arrayReverse = array.split('')
-			arrayReverse.reverse()
-			arrayReverse = arrayReverse.join('')
-			arrayReverse = arrayReverse.split(' ')
+function posSize(message) {
+ //удаление доменов второго уровня
+ let array = `.ru .org .com .academy .accountant .accountants .active 
+	.actor .adult .aero .agency .airforce .apartments .app .archi .army .associates .asia .adult .aero 
+	.attorney .auction .audio .autos .biz .cat .coop .dance 
+	.edu .eus .family .fun .gov .info .int .jobs .mil .mobi .museum .name
+	.net .one .ong .onl .online .ooo
+	.organic .partners .parts .party .pharmacy .photo .photography .photos .physio .pics .pictures .feedback .pink 
+	.pizza .place .plumbing, .plus, .poker .porn .post .press 
+	.pro .productions .properties .property .qpon .racing .recipes .red .rehab .ren .rent .rentals
+	.report .republican .rest .review .reviews .rich .site .tel .trade .travel .xxx .xyz 
+	.yoga .zone,.ninja .art .moe .dev`
 
-			let arrayMessage = message.split('')
-			arrayMessage.reverse()
-			arrayMessage = arrayMessage.join('')
+ let arrayReverse = array.split('')
+ arrayReverse.reverse()
+ arrayReverse = arrayReverse.join('')
+ arrayReverse = arrayReverse.split(' ')
 
-			let result = ''
-			let arrayResult = []
-			let messageDuble = message
+ let arrayMessage = message.split('')
+ arrayMessage.reverse()
+ arrayMessage = arrayMessage.join('')
 
-			let indxEquals = null
+ let result = ''
+ let arrayResult = []
+ let messageDuble = message
 
-			for (let i = 0; i <= arrayReverse.length - 1; i++) {
+ let indxEquals = null
 
-				for (let j = 0; j <= messageDuble.length - 1; j++) {
-					let indx = arrayMessage.indexOf(arrayReverse[i], j)
-					let indxDoth = arrayMessage.indexOf('.', indx)
+ for (let i = 0; i <= arrayReverse.length - 1; i++) {
 
-					if (indx != -1 && arrayMessage[indxDoth + 1] != ' ') {
-						let indxSpace = arrayMessage.indexOf(' ', indx)
-						let str = arrayMessage.slice(indx, indxSpace)
-						str = str.split('')
-						str.reverse()
-						str = str.join('')
-						arrayResult.push(str)
-						indxEquals = indx
-					}
-				} 
-			}
+  for (let j = 0; j <= messageDuble.length - 1; j++) {
+   let indx = arrayMessage.indexOf(arrayReverse[i], j)
+   let indxDoth = arrayMessage.indexOf('.', indx)
 
-			if (indxEquals == null) {
-				console.log(message)
-				return message
-			}
+   if (indx != -1 && arrayMessage[indxDoth + 1] != ' ') {
+    let indxSpace = arrayMessage.indexOf(' ', indx)
+    let str = arrayMessage.slice(indx, indxSpace)
+    str = str.split('')
+    str.reverse()
+    str = str.join('')
+    arrayResult.push(str)
+    indxEquals = indx
+   }
+  }
+ }
 
-			let resultFilter = arrayResult.filter((item, index) => {
-				return arrayResult.indexOf(item) == index
-			})
+ if (indxEquals == null) {
+  console.log(message)
+  return message
+ }
 
-			for (let resultReplace = 0; resultReplace <= resultFilter.length - 1; resultReplace++) {
-				message = message.replace(resultFilter[resultReplace], `<a href="${resultFilter[resultReplace]}">${resultFilter[resultReplace]}</a>`)
-				result = message
-			}
+ let resultFilter = arrayResult.filter((item, index) => {
+  return arrayResult.indexOf(item) == index
+ })
 
-			console.log(result)
-			return result
-		}
-		posSize('Привет! fdsfds.porn asdas.porn dsfdsfhj.com');
+ for (let resultReplace = 0; resultReplace <= resultFilter.length - 1; resultReplace++) {
+  message = message.replace(resultFilter[resultReplace], `<a href="${resultFilter[resultReplace]}">${resultFilter[resultReplace]}</a>`)
+  result = message
+ }
+
+ console.log(result)
+ return result
+}
+posSize('Привет! fdsfds.porn asdas.porn dsfdsfhj.com');
 
--- script.js
+++ script.js
@@ -1,44 +1,44 @@
- function posSize(message) {
-		//удаление доменов второго уровня
-		let array = `.ru .org .com .academy .accountant .accountants .active 
-		.actor .adult .aero .agency .airforce .apartments .app .archi .army .associates .asia .adult .aero 
-		.attorney .auction .audio .autos .biz .cat .coop .dance 
-		.edu .eus .family .fun .gov .info .int .jobs .mil .mobi .museum .name
-		.net .one .ong .onl .online .ooo
-		.organic .partners .parts .party .pharmacy .photo .photography .photos .physio .pics .pictures .feedback .pink 
-		.pizza .place .plumbing, .plus, .poker .porn .post .press 
-		.pro .productions .properties .property .qpon .racing .recipes .red .rehab .ren .rent .rentals
-		.report .republican .rest .review .reviews .rich .site .tel .trade .travel .xxx .xyz 
-		.yoga .zone,.ninja .art .moe .dev`
-		
-		let arrayReverse = array.split('')
-		arrayReverse.reverse()
-		arrayReverse = arrayReverse.join('')
-		arrayReverse = arrayReverse.split(' ')
+function posSize(message) {
+ //удаление доменов второго уровня
+ let array = `.ru .org .com .academy .accountant .accountants .active 
+	.actor .adult .aero .agency .airforce .apartments .app .archi .army .associates .asia .adult .aero 
+	.attorney .auction .audio .autos .biz .cat .coop .dance 
+	.edu .eus .family .fun .gov .info .int .jobs .mil .mobi .museum .name
+	.net .one .ong .onl .online .ooo
+	.organic .partners .parts .party .pharmacy .photo .photography .photos .physio .pics .pictures .feedback .pink 
+	.pizza .place .plumbing, .plus, .poker .porn .post .press 
+	.pro .productions .properties .property .qpon .racing .recipes .red .rehab .ren .rent .rentals
+	.report .republican .rest .review .reviews .rich .site .tel .trade .travel .xxx .xyz 
+	.yoga .zone,.ninja .art .moe .dev`
 
-		let arrayMessage = message.split('')
-		arrayMessage.reverse()
-		arrayMessage = arrayMessage.join('')
+ let arrayReverse = array.split('')
+ arrayReverse.reverse()
+ arrayReverse = arrayReverse.join('')
+ arrayReverse = arrayReverse.split(' ')
 
-		let result = 0
+ let arrayMessage = message.split('')
+ arrayMessage.reverse()
+ arrayMessage = arrayMessage.join('')
 
-		for (let i = 0; i <= arrayReverse.length - 1; i++) {
-			let indx = arrayMessage.indexOf(arrayReverse[i], 0)
-			let indxDoth = arrayMessage.indexOf('.', indx)
-			
+ let result = 0
 
-			if (indx != -1 && arrayMessage[indxDoth + 1] != ' ' ) {
-				let indxSpace = arrayMessage.indexOf(' ', indx)
-				let str = arrayMessage.slice(indx, indxSpace)
-				result += str.length
-			}
-		}
+ for (let i = 0; i <= arrayReverse.length - 1; i++) {
+  let indx = arrayMessage.indexOf(arrayReverse[i], 0)
+  let indxDoth = arrayMessage.indexOf('.', indx)
 
-		result = message.length - result
-		console.log(result)
-		return result
-	}
-	posSize('Привет! https://github.com Привет, . http://github.racing, Пока www.github.xxx привет мир https://github.ru');
+
+  if (indx != -1 && arrayMessage[indxDoth + 1] != ' ') {
+   let indxSpace = arrayMessage.indexOf(' ', indx)
+   let str = arrayMessage.slice(indx, indxSpace)
+   result += str.length
+  }
+ }
+
+ result = message.length - result
+ console.log(result)
+ return result
+}
+posSize('Привет! https://github.com Привет, . http://github.racing, Пока www.github.xxx привет мир https://github.ru');
 
 // function deleteProt (message) {
 // 	Уделение с протоколом
@@ -68,12 +68,12 @@
 //    str = message.slice(indxHttps);
 //    indxResult = str.indexOf(' ');
 //    str = str.slice(0, indxResult + 1);
-//    message = message.replace(str, ''); 
+//    message = message.replace(str, '');
 //    result = message.length;
 //   } else if (indxHttp > -1) {
 //    str = message.slice(indxHttp);
-//    indxResult = str.indexOf(' '); 
-//    str = str.slice(0, indxResult + 1); 
+//    indxResult = str.indexOf(' ');
+//    str = str.slice(0, indxResult + 1);
 //    message = message.replace(str, '');
 //    result = message.length;
 //   } else if (indxWww > -1) {
