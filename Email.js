85d3ed80802f9f8b310f8973592ea067eb642694
Тимур <timur-skvorcov-666@mail.ru>
2023-01-31 09:38:24 +0500

    изменена функция валидации email

--- Email.js
+++ Email.js
@@ -1,34 +1,58 @@
-let strMail = `@hotmail.com;@mail.ru;@inbox.ru;@list.ru;@bk.ru;@internet.ru;@yandex.ru;@gmail.com;@yahoo.com;@hotmail.com;@outlook.com;@rambler.ru;
-@myrambler.ru;@autorambler.ru;@lenta.ru;@rambler.ua;@ro.ru`
-let domen = ['.com', '.ru']
-let array = strMail.split(';')
+let array = `.ru .org .com .academy .accountant .accountants .active 
+.actor .adult .aero .agency .airforce .apartments .app .archi .army .associates .asia .adult .aero 
+.attorney .auction .audio .autos .biz .cat .coop .dance 
+.edu .eus .family .fun .gov .info .int .jobs .mil .mobi .museum .name
+.net .one .ong .onl .online .ooo
+.organic .partners .parts .party .pharmacy .photo .photography .photos .physio .pics .pictures .feedback .pink 
+.pizza .place .plumbing .plus .poker .porn .post .press 
+.pro .productions .properties .property .qpon .racing .recipes .red .rehab .ren .rent .rentals
+.report .republican .rest .review .reviews .rich .site .tel .trade .travel .xxx .xyz 
+.yoga .zone .ninja .art .moe .dev`
 
-let result = null
-//функция валидации email
+array = array.split(' ')
+
 function valid(strListMail) {
- //цикл перебора возможных вариантова email
- let arr = array.forEach((item, index) => {
-  let str = array[index]
-  //цикл перебора основной строки
-  for (let j = 0; j <= strListMail.length - 1; j++) {
-   let indx2 = strListMail.indexOf(str)
-   //условие при котором почта валидна
-   if (indx2 != -1) {
+ let strSlice = ''
+ let res = 0
 
-    for (let domenEnd = 0; domenEnd <= domen.length - 1; domenEnd++) {
-     let indx3 = strListMail.indexOf('', strListMail.length)
+ for (let j = 0; j <= strListMail.length - 1; j++) {
+  let indx = strListMail.indexOf('@', j)
 
-     if (strListMail[indx3 - 2] == 'r' || strListMail[indx3 - 2] == 'o') {
-      result = true
-     }
-    }
-   }
+  if (indx != -1) {
+   strSlice = strListMail.slice(indx)
+   strSlice = strSlice
   }
- })
+ }
 
- console.log(result)
- return result == true
+ for (let k = 0; k <= strSlice.length - 1; k++) {
+  let indxDown = strListMail.indexOf('@')
+  let indx = strListMail.indexOf('', strListMail.length)
+  let indxUp = strListMail.indexOf('.')
+  if (strListMail[indx - 2] != strListMail[indx - 1] && strListMail[indxUp] != strListMail[indxUp + 1] && strListMail[indxDown] != strListMail[indxDown + 1]) {
+
+   let arr = array.forEach((item, index) => {
+    let arr2 = array.forEach((item, index) => {
+     let indx = strSlice.indexOf(array[index])
+     if (indx != -1) {
+      res = 1
+     }
+    })
+   })
+  }
+ }
+
+ if (res == 1) {
+  console.log(true)
+  return true
+ } else {
+  console.log(false)
+  return false
+ }
 }
 
 valid('timur-sdsada@outlook.com')
+valid('timur-sdsada@burtovoy.org')
+valid('timur-sdsada@outlook.sidxte')
+valid('timur-sdsada@outlook.dancee')
+
 
