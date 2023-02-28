68a129374d728c1148eed8d483545cf61b9f4b28
Timur <timur-skvorcov-666@mail.ru>
2023-02-28 18:09:06 +0500

    модули

---
+++ js/posSize.js
@@ -0,0 +1,44 @@
+//подсчёт символов без ссылок
+export function posSize(message) {
+ //удаление доменов второго уровня
+ let array = `.ru .org .com .academy .accountant .accountants .active 
+ .actor .adult .aero .agency .airforce .apartments .app .archi .army .associates .asia .adult .aero 
+ .attorney .auction .audio .autos .biz .cat .coop .dance 
+ .edu .eus .family .fun .gov .info .int .jobs .mil .mobi .museum .name
+ .net .one .ong .onl .online .ooo
+ .organic .partners .parts .party .pharmacy .photo .photography .photos .physio .pics .pictures .feedback .pink 
+ .pizza .place .plumbing .plus .poker .porn .post .press 
+ .pro .productions .properties .property .qpon .racing .recipes .red .rehab .ren .rent .rentals
+ .report .republican .rest .review .reviews .rich .site .tel .trade .travel .xxx .xyz 
+ .yoga .zone .ninja .art .moe .dev` 
+
+ let arrayReverse = array.split('')
+ arrayReverse.reverse()
+ arrayReverse = arrayReverse.join('')
+ arrayReverse = arrayReverse.split(' ')
+
+ let arrayMessage = message.split('')
+ arrayMessage.reverse()
+ arrayMessage = arrayMessage.join('')
+
+ let result = 0
+
+ let arr = arrayReverse.forEach((item, index) => {
+  let indx = arrayMessage.indexOf(arrayReverse[index], 0)
+  let indxDoth = arrayMessage.indexOf('.', indx)
+
+  if (indx != -1 && arrayMessage[indxDoth + 1] != ' ') {
+   let indxSpace = arrayMessage.indexOf(' ', indx)
+   let str = arrayMessage.slice(indx, indxSpace)
+   result += str.length
+  }
+
+ })
+
+ result = message.length - result
+ console.log(result)
+ return result
+}
+posSize('Привет! https://github.com Привет, . http://github.racing, Пока www.github.xxx привет мир https://github.ru');
+
+
---
+++ js/timeBack.js
@@ -0,0 +1,57 @@
+//ФУНКЦИЯ ВРЕМЕНИ
+export let timeBack = (time) => {
+  let minute = 60;
+  let hour = 1440;
+  let day = 525600;
+  
+  if (time < minute) {
+    if (String(time)[String(time).length - 1] == '1' && time != 11) {
+      return `${time} минуту назад`
+    } else if (String(time)[String(time).length - 1] == '2' && String(time)[String(time).length - 2] != '1' || String(time)[String(time).length - 1] == '3' && String(time)[String(time).length - 2] != '1' || String(time)[String(time).length - 1] == '4' && String(time)[String(time).length - 2] != '1') {
+      return `${time} минуты назад`
+    } else {
+      return `${time} минут назад`
+    }
+  } else if (time >= minute && time < hour) {
+    let timeHour = time / 60
+    timeHour = Math.trunc(timeHour)
+    let times = time - (60 * timeHour)
+    if (times < 30) {
+      if (String(timeHour)[String(timeHour).length - 1] == '1' && String(timeHour)[String(timeHour).length - 2] != '1') {
+        return `${timeHour} час назад`
+      } else if (String(timeHour)[String(timeHour).length - 1] == '2' && String(timeHour)[String(timeHour).length - 2] != '1' || String(timeHour)[String(timeHour).length - 1] == '3' && String(timeHour)[String(timeHour).length - 2] != '1' || String(timeHour)[String(timeHour).length - 1] == '4' && String(timeHour)[String(timeHour).length - 2] != '1') {
+        return `${timeHour} часа назад`
+      } else {
+        return `${timeHour} часов назад`
+      }
+    } else if (times >= 30) {
+      timeHour += 1
+      if (String(timeHour)[String(timeHour).length - 1] == '1' && String(timeHour)[String(timeHour).length - 2] != '1') {
+        return `${timeHour} час назад`
+      } else if (String(timeHour)[String(timeHour).length - 1] == '2' && String(timeHour)[String(timeHour).length - 2] != '1' || String(timeHour)[String(timeHour).length - 1] == '3' && String(timeHour)[String(timeHour).length - 2] != '1' || String(timeHour)[String(timeHour).length - 1] == '4' && String(timeHour)[String(timeHour).length - 2] != '1') {
+        return `${timeHour} часа назад`
+      } else {
+        return `${timeHour} часов назад`
+      }
+    }
+  } else if (time <= day && time >= hour) {
+    let timeDay = time / 1440
+    timeDay = Math.trunc(timeDay)
+    if (String(timeDay)[String(timeDay).length - 1] == '1' && String(timeDay)[String(timeDay).length - 2] != '1') {
+      return `${timeDay} день назад`
+    } else if (String(timeDay)[String(timeDay).length - 1] == '2' && String(timeDay)[String(timeDay).length - 2] != '1' || String(timeDay)[String(timeDay).length - 1] == '3' && String(timeDay)[String(timeDay).length - 2] != '1' || String(timeDay)[String(timeDay).length - 1] == '4' && String(timeDay)[String(timeDay).length - 2] != '1') {
+      return `${timeDay} дня назад`
+    } else {
+      return `${timeDay} дней назад`
+    }
+  } else {
+    return 'более года назад'
+  }
+
+}
+console.log(timeBack(10))
+console.log(timeBack(60))
+console.log(timeBack(120))
+console.log(timeBack(1440))
+console.log(timeBack(525400))
+console.log(timeBack(525601))
