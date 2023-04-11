42e154ec845da2c869795251cfcf49f425703512
Timur <timur-skvorcov-666@mail.ru>
2023-04-11 14:30:18 +0500

    корректировки

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,10 @@
 const headerFormAutoriz = document.querySelector('.header__form-autoriz');
-const footerFormAutoriz = document.querySelector('.footer__form-autoriz')
+const footerFormAutoriz = document.querySelector('.footer__form-autoriz');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__name-images');
 const body = document.querySelector('body');
-const formContent = document.querySelectorAll('.name__content')
-const formRegistr = document.querySelectorAll('.fr')
+const formContent = document.querySelectorAll('.name__content');
+const formRegistr = document.querySelectorAll('.fr');
 
 function bodyLock() {
   let scrollWidth = window.innerWidth - document.querySelector('body').offsetWidth;
@@ -21,40 +21,40 @@ function bodyLock() {
   }
 }
 
+function keyboard() {
+  for (let i = 0; i <= formRegistr.length - 1; i += 1) {
+    formRegistr[i].addEventListener('keydown', () => {
+      const contentInput = formRegistr[i].value;
+
+      if (contentInput !== '') {
+        formContent[i].setAttribute('style', 'opacity: 1; visibility: visible;');
+      } else {
+        formContent[i].setAttribute('style', 'opacity: 0; visibility: hidden;');
+      }
+    });
+  }
+}
+
 function popupOpenHead() {
   bodyLock();
   popup.setAttribute('style', 'opacity: 1; visibility: visible;');
-  keyboard()
+  keyboard();
 }
 
 function popupOpenFooter() {
-  popupOpenHead()
-  keyboard()
+  popupOpenHead();
+  keyboard();
 }
 
 function popupCloses() {
   window.addEventListener('click', (e) => {
     if (e.target === popup || e.target === popupClose) {
-      e.preventDefault()
+      e.preventDefault();
       bodyLock();
       popup.setAttribute('style', 'opacity: ; visibility: hidden;');
       body.setAttribute('class', 'false');
     }
   });
-}
-
-function keyboard() {
-  for (let i = 0; i <= formRegistr.length - 1; i++) {
-    formRegistr[i].addEventListener('keydown', () => {
-      const contentInput = formRegistr[i].value
-      console.log(contentInput)
-      if (contentInput != '') {
-        formContent[i].setAttribute('style', 'opacity: 1; visibility: visible;')
-      } else {
-        formContent[i].setAttribute('style', 'opacity: 0; visibility: hidden;')
-      }
-    })
-  }
 }
 
 headerFormAutoriz.addEventListener('click', (event) => {
@@ -63,9 +63,8 @@ headerFormAutoriz.addEventListener('click', (event) => {
 
 footerFormAutoriz.addEventListener('click', (e) => {
   e.preventDefault();
-})
+});
 
 popupCloses();
 headerFormAutoriz.addEventListener('click', popupOpenHead);
 footerFormAutoriz.addEventListener('click', popupOpenFooter);
-
