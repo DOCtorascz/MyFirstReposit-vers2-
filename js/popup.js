c235b8d190f86374de7027339155ab1fff3ff220
Timur <timur-skvorcov-666@mail.ru>
2023-04-11 14:26:44 +0500

    адаптив попап

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,10 @@
 const headerFormAutoriz = document.querySelector('.header__form-autoriz');
+const footerFormAutoriz = document.querySelector('.footer__form-autoriz')
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__name-images');
 const body = document.querySelector('body');
+const formContent = document.querySelectorAll('.name__content')
+const formRegistr = document.querySelectorAll('.fr')
 
 function bodyLock() {
   let scrollWidth = window.innerWidth - document.querySelector('body').offsetWidth;
@@ -18,15 +21,21 @@ function bodyLock() {
   }
 }
 
-function popupOpen() {
+function popupOpenHead() {
   bodyLock();
-
   popup.setAttribute('style', 'opacity: 1; visibility: visible;');
+  keyboard()
+}
+
+function popupOpenFooter() {
+  popupOpenHead()
+  keyboard()
 }
 
 function popupCloses() {
   window.addEventListener('click', (e) => {
     if (e.target === popup || e.target === popupClose) {
+      e.preventDefault()
       bodyLock();
       popup.setAttribute('style', 'opacity: ; visibility: hidden;');
       body.setAttribute('class', 'false');
@@ -34,9 +43,29 @@ function popupCloses() {
   });
 }
 
+function keyboard() {
+  for (let i = 0; i <= formRegistr.length - 1; i++) {
+    formRegistr[i].addEventListener('keydown', () => {
+      const contentInput = formRegistr[i].value
+      console.log(contentInput)
+      if (contentInput != '') {
+        formContent[i].setAttribute('style', 'opacity: 1; visibility: visible;')
+      } else {
+        formContent[i].setAttribute('style', 'opacity: 0; visibility: hidden;')
+      }
+    })
+  }
+}
+
 headerFormAutoriz.addEventListener('click', (event) => {
   event.preventDefault();
 });
 
+footerFormAutoriz.addEventListener('click', (e) => {
+  e.preventDefault();
+})
+
 popupCloses();
-headerFormAutoriz.addEventListener('click', popupOpen);
+headerFormAutoriz.addEventListener('click', popupOpenHead);
+footerFormAutoriz.addEventListener('click', popupOpenFooter);
+
