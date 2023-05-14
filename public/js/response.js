305fe744368ac44f36eb8db042e5ec1b47f3bff0
Timur <timur-skvorcov-666@mail.ru>
2023-05-14 07:19:34 +0500

    lastmessage

--- public/js/response.js
+++ public/js/response.js
@@ -1,13 +1,15 @@
 const mainStateNum = document.querySelectorAll('.main-state__num');
 const mainStateName = document.querySelectorAll('.main-state__name');
-const commentsAccountName = document.querySelectorAll('.comments__account-name');
-const commentsTimeBack = document.querySelectorAll('.comments__time-back');
-const commentsText = document.querySelectorAll('.comments__text');
-const accountPage = document.querySelectorAll('.account-page');
+// const commentsAccountName = document.querySelectorAll('.comments__account-name');
+// const commentsTimeBack = document.querySelectorAll('.comments__time-back');
+// const commentsText = document.querySelectorAll('.comments__text');
+// const accountPage = document.querySelectorAll('.account-page');
+const commetsPersone = document.querySelector('.commets__persone');
 
 function response() {
   const url = 'data.json';
-  const urlPages = 'pictures.json';
+  // const urlPages = 'pictures.json';
+  let strLastMessage = '';
 
   fetch(url)
     .then((respon) => respon.json())
@@ -24,63 +26,31 @@ function response() {
           mainStateName[i].textContent = element.nameCell;
         }
       });
-      info.lastMessages.forEach((element, i) => {
-        if (i === 0) {
-          commentsAccountName[0].children[0].textContent = element.namePerson;
-          commentsAccountName[0].children[1].textContent = element.login;
-          commentsTimeBack[0].children[0].textContent = element.timeAgo;
-          commentsText[0].children[0].textContent = element.comment;
-          commentsText[0].children[1].children[1].textContent = element.repost;
-          commentsText[0].children[1].children[3].textContent = element.like;
-          commentsText[0].children[1].children[5].textContent = element.loaded;
-        } else if (i === 1) {
-          commentsAccountName[i].children[0].textContent = element.namePerson;
-          commentsAccountName[i].children[1].textContent = element.login;
-          commentsTimeBack[i].children[0].textContent = element.timeAgo;
-          commentsText[i].children[0].textContent = element.comment;
-          commentsText[i].children[1].children[1].textContent = element.repost;
-          commentsText[i].children[1].children[3].textContent = element.like;
-          commentsText[i].children[1].children[5].textContent = element.loaded;
-        } else if (i === 2) {
-          commentsAccountName[i].children[0].textContent = element.namePerson;
-          commentsAccountName[i].children[1].textContent = element.login;
-          commentsTimeBack[i].children[0].textContent = element.timeAgo;
-          commentsText[i].children[0].textContent = element.comment;
-          commentsText[i].children[2].children[1].textContent = element.repost;
-          commentsText[i].children[2].children[3].textContent = element.like;
-          commentsText[i].children[2].children[5].textContent = element.loaded;
-        } else if (i === 3) {
-          commentsAccountName[i].children[0].textContent = element.namePerson;
-          commentsAccountName[i].children[1].textContent = element.login;
-          commentsTimeBack[i].children[0].textContent = element.timeAgo;
-          commentsText[i].children[0].textContent = element.comment;
-          commentsText[i].children[1].children[1].textContent = element.repost;
-          commentsText[i].children[1].children[3].textContent = element.like;
-          commentsText[i].children[1].children[5].textContent = element.loaded;
-        }
+      info.lastMessages.forEach((element) => {
+        strLastMessage += `<div class="comments__account">
+        <img class="account-page" src="${element.imgUrl}" alt="account">
+        <div class="comments__page">
+          <div class="comments__info-account">
+            <div class="comments__account-name">
+              <p>${element.namePerson}</p>
+              <p>${element.login}</p>
+            </div>
+            <div class="comments__time-back">
+              <p>${element.timeAgo}</p>
+            </div>
+          </div>
+          <div class="comments__text">
+            <p>${element.comment}</p>
+            <div class="comments__state">
+              <img src="${element.imgLike}" alt="twit"><span>${element.repost}</span>
+              <img src="${element.imgLoaded}" alt="twit"><span>${element.like}</span>
+              <img src="${element.imgRepost}" alt="twit"><span>${element.loaded}</span>
+            </div>
+          </div>
+          </div>
+        </div>`;
       });
-    });
-
-  fetch(urlPages)
-    .then((respon) => respon.json())
-    .then((info) => {
-      info.picture.forEach((element, i) => {
-        if (i === 1) {
-          accountPage[0].setAttribute('src', `${element.imgUrl}`);
-        } else if (i === 2) {
-          accountPage[1].setAttribute('src', `${element.imgUrl}`);
-        } else if (i === 3) {
-          accountPage[2].setAttribute('src', `${element.imgUrl}`);
-        } else if (i === 4) {
-          accountPage[3].setAttribute('src', `${element.imgUrl}`);
-        }
-
-        if (i === 0) {
-          commentsText[0].children[1].children[0].setAttribute('src', `${element.imgRepost}`);
-          commentsText[0].children[1].children[2].setAttribute('src', `${element.imgLike}`);
-          commentsText[0].children[1].children[4].setAttribute('src', `${element.imgLoaded}`);
-        }
-      });
+      commetsPersone.insertAdjacentHTML('beforeend', `${strLastMessage}`);
     });
 }
 
