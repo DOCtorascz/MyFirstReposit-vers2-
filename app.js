6fef488af8961627dff298e27bb39f9aae435929
Timur <timur-skvorcov-666@mail.ru>
2023-06-16 21:12:47 +0500

    express

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
-import pkg from 'pg';
+// import pkg from 'pg';
+import express from 'express';
+import path from 'path';
 
-const { Pool, Client } = pkg;
-const connectionString = 'postgres://twitter_production_x6ag_user:ucxxPWmNTUve1Sg91oXF89qYbVNJ21EO@dpg-chqrnne7avjb90n96u6g-a.oregon-postgres.render.com/twitter_production_x6ag?ssl=true';
+const dirname = path.resolve();
 
-const pool = new Pool({
-  connectionString,
+const app = express();
+const port = process.env.PORT || 5501;
+
+app.use(express.static(path.resolve(dirname, 'public')));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// const { Pool, Client } = pkg;
+// const connectionString = 'postgres://twitter_production_x6ag_user:ucxxPWmNTUve1Sg91oXF89qYbVNJ21EO@dpg-chqrnne7avjb90n96u6g-a.oregon-postgres.render.com/twitter_production_x6ag?ssl=true';
+
+// const pool = new Pool({
+//   connectionString,
+// });
+
+// await pool.end();
+
+// const client = new Client({
+//   connectionString,
+// });
+
+// await client.connect();
+
+// const objSelect = await client.query('SELECT *FROM mess');
+
+// await client.query('SELECT NOW() as now');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(dirname, 'public', 'index.html'));
 });
 
-await pool.end();
+// app.get('/posts.json', (req, res) => {
+//   res.type('json').send(objSelect.rows);
+// });
 
-const client = new Client({
-  connectionString,
-});
-
-await client.connect();
-
-const objSelect = await client.query('SELECT *FROM mess');
-
-await client.query('SELECT NOW() as now');
-
-const selectStr = JSON.stringify(objSelect.rows);
-console.log(selectStr);
-
-await client.end();
+// await client.end();
